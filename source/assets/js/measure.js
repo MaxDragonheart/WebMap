f85e72a069be8357688c40c00a22441eb365c081
MaxDragonheart <massimilianomoraca@gmail.com>
2022-08-01 13:02:32 +0200

    los glacieres

--- source/assets/js/measure.js
+++ source/assets/js/measure.js
@@ -1,52 +1,87 @@
-const source = new ol.source.Vector({wrapX: false});
+/**
+ * Format length output.
+ * @param {LineString} line The line.
+ * @return {string} The formatted length.
+ */
+const formatLength = function (line) {
+  const length = ol.sphere.getLength(line);
+  let output;
+  if (length > 100) {
+    output = Math.round((length / 1000) * 100) / 100 + ' ' + 'km';
+  } else {
+    output = Math.round(length * 100) / 100 + ' ' + 'm';
+  }
+  return output;
+};
 
-const vector = new ol.layer.Vector({
-    source: source,
-    style: new ol.style.Style({
+/**
+ * Format area output.
+ * @param {Polygon} polygon The polygon.
+ * @return {string} Formatted area.
+ */
+const formatArea = function (polygon) {
+  const area = ol.sphere.getArea(polygon);
+  let output;
+  if (area > 10000) {
+    output = Math.round((area / 1000000) * 100) / 100 + ' ' + 'km<sup>2</sup>';
+  } else {
+    output = Math.round(area * 100) / 100 + ' ' + 'm<sup>2</sup>';
+  }
+  return output;
+};
+
+const source = new ol.source.Vector();
+
+let draw; // global so we can remove it later
+
+vectorStyle = new ol.style.Style({
+  fill: new ol.style.Fill({
+    color: 'rgba(255, 255, 255, 0.2)',
+  }),
+  stroke: new ol.style.Stroke({
+    color: 'rgba(0, 0, 0, 0.5)',
+    lineDash: [10, 10],
+    width: 2,
+  }),
+  image: new ol.style.Circle({
+    radius: 5,
+    stroke: new ol.style.Stroke({
+      color: 'rgba(0, 0, 0, 0.7)',
+    }),
     fill: new ol.style.Fill({
       color: 'rgba(255, 255, 255, 0.2)',
     }),
-    stroke: new ol.style.Stroke({
-      color: '#ffcc33',
-      width: 2,
-    }),
-    image: new ol.style.Circle({
-      radius: 7,
-      fill: new ol.style.Fill({
-        color: '#ffcc33',
-      }),
-    }),
   }),
 });
-map.addLayer(vector);
 
-let draw; // global so we can remove it later
-function addInteractionLineString() {
+function addInteraction(geomType) {
 
-    draw = new ol.interaction.Draw({
-      source: source,
-      type: 'LineString',
-      style: new ol.style.Style({
-        fill: new ol.style.Fill({
-          color: 'rgba(255, 255, 255, 0.2)',
-        }),
-        stroke: new ol.style.Stroke({
-          color: 'rgba(0, 0, 0, 0.5)',
-          lineDash: [10, 10],
-          width: 2,
-        }),
-        image: new ol.style.Circle({
-          radius: 5,
-          stroke: new ol.style.Stroke({
-            color: 'rgba(0, 0, 0, 0.7)',
-          }),
-          fill: new ol.style.Fill({
-            color: 'rgba(255, 255, 255, 0.2)',
-          }),
-        }),
-      }),
+  console.log(geomType)
+
+  draw = new ol.interaction.Draw({
+    source: source,
+    type: geomType,
+    style: vectorStyle,
+  });
+
+  let listener;
+  draw.on('drawstart', function (evt) {
+    // set sketch
+    sketch = evt.feature;
+
+    listener = sketch.getGeometry().on('change', function (evt) {
+        const geom = evt.target;
+        //let output;
+        if (geom instanceof ol.geom.Polygon) {
+          output = formatArea(geom);
+          document.getElementById("area").innerText = output;
+        } else if (geom instanceof ol.geom.LineString) {
+          output = formatLength(geom);
+          document.getElementById("length").innerText = output;
+        }
+
     });
 
-}
+  });
 
-addInteractionLineString();
+}
